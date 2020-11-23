3cd479e079535758df0dfbad46ca43632f001bc7
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-23 07:26:15 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'taja-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'test-angular-jwt-auth';
+}
