085bdf52205cea6be87cf4f2f410a60d897695d2
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-24 12:49:12 +0000

    hello, login and welcome components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [{
-  path: '',
-  loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule)
-},
-{
-  path: 'test',
-  loadChildren: () => import('./test/test.module').then(m => m.TestModule)
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'hello',
+    loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule)
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('./test/test.module').then(m => m.TestModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
