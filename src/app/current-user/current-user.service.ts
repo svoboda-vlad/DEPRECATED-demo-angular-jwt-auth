d1b5186003ca4d13f0686e865c9a619c493bbef7
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-03-26 05:13:13 +0000

    current user caching

--- src/app/current-user/current-user.service.ts
+++ src/app/current-user/current-user.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,22 @@ import { environment } from 'src/environments/environment';
 export class CurrentUserService {
 
   currentUserUrl = 'current-user';
+  currentUser: UserInfo = null;
 
   constructor(private http: HttpClient) { }
 
   getCurrentUser(jwtToken: string) : Observable<UserInfo> {
+    if (this.currentUser) {
+      return of(this.currentUser);
+    }
+
     const options = {
       headers: new HttpHeaders({
         Authorization: jwtToken
       })
     };
-    return this.http.get<UserInfo>(environment.SERVER_URL + this.currentUserUrl, options);
+    return this.http.get<UserInfo>(environment.SERVER_URL + this.currentUserUrl, options)
+    .pipe(map(res => this.currentUser = res));
   }
 
 }
