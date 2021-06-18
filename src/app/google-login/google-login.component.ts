93481ed554cdd7d58fcddcb584a6618b3988b4a7
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-06-18 09:42:48 +0000

    google login working

--- src/app/google-login/google-login.component.ts
+++ src/app/google-login/google-login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
+import { CurrentUserService } from '../current-user/current-user.service';
+import { GoogleLoginService, IdTokenGoogle } from './google-login.service';
 
 @Component({
   selector: 'daja-google-login',
@@ -7,20 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleLoginComponent implements OnInit {
 
-  constructor() { }
+  accessTokenKey = "access_token";
+  googleLoginError = false;
+  googleLoginSubscription: Subscription;
 
-  accessToken = "";
-  idToken = "";
+  constructor(private router: Router,
+    private googleLoginService: GoogleLoginService,
+    private currentUserService: CurrentUserService) { }
 
   ngOnInit(): void {
+    let accessToken = "";
+    let idToken = "";
     const hashParams = location.hash.replace("#","").split("&");
     hashParams.forEach((param) => {
       const p = param.split("=");
-      if (p[0] == "access_token") this.accessToken = p[1];
-      if (p[0] == "id_token") this.idToken = p[1];
+      if (p[0] == "access_token") accessToken = p[1];
+      if (p[0] == "id_token") idToken = p[1];
     });
-    console.log("access_token: ", this.accessToken);
-    console.log("id_token: ", this.idToken);
+    localStorage.setItem(this.accessTokenKey, accessToken);
+    const idTokenGoogle: IdTokenGoogle = new IdTokenGoogle(idToken);
+
+    this.googleLoginSubscription = this.googleLoginService
+      .logIn(idTokenGoogle)
+      .pipe(
+        concatMap(() => this.currentUserService.getCurrentUser())
+      ).subscribe(() => {
+        this.googleLoginError = false;
+        this.router.navigate(['']);
+      },
+      () => this.googleLoginError = true);
+
   }
 
 }
