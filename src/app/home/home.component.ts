585a33e9f3c690cda7aa20627cd3e37f4ad0edc0
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-06-29 06:51:47 +0000

    redirect to login when not logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { CurrentUserService, CurrentUser } from '../current-user/current-user.service';
 
 @Component({
@@ -9,12 +11,15 @@ import { CurrentUserService, CurrentUser } from '../current-user/current-user.se
 })
 export class HomeComponent implements OnInit {
 
-  currentUser$: Observable<CurrentUser> = null;
+  currentUser$: Observable<CurrentUser | boolean> = null;
 
-  constructor(private currentUserService: CurrentUserService) { }
+  constructor(private router: Router, private currentUserService: CurrentUserService) { }
 
   ngOnInit(): void {
-    this.currentUser$ = this.currentUserService.getCurrentUser();
+    this.currentUser$ = this.currentUserService.getCurrentUser()
+      .pipe(
+        catchError(() => this.router.navigate(['/login']))
+      );
   }
 
 }
