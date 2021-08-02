1d5fa5f78f8d85f5e55e899d67977462d7c2fb00
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-08-02 12:39:20 +0000

    exchange rate detail component added

---
+++ src/app/exchange-rate-detail/exchange-rate-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'daja-exchange-rate-detail',
+  templateUrl: './exchange-rate-detail.component.html',
+  styleUrls: ['./exchange-rate-detail.component.scss']
+})
+export class ExchangeRateDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
