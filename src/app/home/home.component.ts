df7082e2e7e17732f0279cb47800a0a07aff19fe
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-08-06 05:34:07 +0000

    invalid jwt token logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { LoginService } from '../login/login.service';
 import { UserService, User } from '../user/user.service';
 
 @Component({
@@ -13,12 +14,17 @@ export class HomeComponent implements OnInit {
 
   currentUser$: Observable<User | boolean> = null;
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService, private loginService: LoginService) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.userService.getCurrentUser()
       .pipe(
-        catchError(() => this.router.navigate(['/login']))
+        catchError((err) => {
+          this.loginService.logOut();
+          this.userService.logOut();
+          this.router.navigate(['/login']);
+          return throwError(err);
+        })
       );
   }
 
